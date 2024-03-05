6d85fb6b0736a5afe7e5adf096449cf03403b0b3
hectorHumanDev <113396173+MF-H0d@users.noreply.github.com>
2024-03-05 10:55:20 -0700

    useState

---
+++ src/features/campsites/campsitesSlice.js
@@ -0,0 +1,14 @@
+import { CAMPSITES } from "../../app/shared/CAMPSITES";
+
+export const selectAllCampsites = () => {
+  return CAMPSITES;
+};
+
+// export const selectRandomCampsite = () => {
+//   const index = Math.floor(Math.random() * CAMPSITES.length);
+//   return CAMPSITES[index];
+// };
+
+export const selectCampsiteById = (id) => {
+  return CAMPSITES.find((campsite) => campsite.id === id);
+};
